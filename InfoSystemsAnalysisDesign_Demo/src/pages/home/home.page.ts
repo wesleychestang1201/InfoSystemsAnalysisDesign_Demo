8099df6ddc39cdd8f870b04068a9850bdec97602
Wesley Chestang <wesley.chestang@terraforms.net>
2019-04-16 01:16:27 -0500

    Angular Card Selection
    Implemented the home page re-styling when the "AngularCard" is clicked on.

--- InfoSystemsAnalysisDesign_Demo/src/pages/home/home.page.ts
+++ InfoSystemsAnalysisDesign_Demo/src/pages/home/home.page.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
+
+
+  //Clicking the "AngularCard" calles the function
+  restyleHomepage() {
+    //Change properties of these elements which are refered to by their ID value
+    document.getElementById("pageTitle").innerText = "ANGULAR !"
+    document.getElementById("angularCard").style.boxShadow = "0px 0px 100px 40px #FABC02"
+
+    //Changing the display style will designate whether a given element exists on the page or not
+    document.getElementById("clickMeButton").style.display = "block"
+    document.getElementById("mongoCard").style.display = "none"
+    document.getElementById("expressCard").style.display = "none"
+    document.getElementById("nodeCard").style.display = "none"
+  }
+
 }
