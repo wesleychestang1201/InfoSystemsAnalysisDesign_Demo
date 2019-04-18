3f324bca274600889e7248aa52d42eef7c4631a6
Wesley Chestang <wesley.chestang@terraforms.net>
2019-04-18 00:05:07 -0500

    Click Me Button Animation Added

--- InfoSystemsAnalysisDesign_Demo/src/pages/angular-directives-explanation/angular-directives-explanation.page.ts
+++ InfoSystemsAnalysisDesign_Demo/src/pages/angular-directives-explanation/angular-directives-explanation.page.ts
@@ -5,11 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './angular-directives-explanation.page.html',
   styleUrls: ['./angular-directives-explanation.page.scss'],
 })
-export class AngularDirectivesExplanationPage implements OnInit {
+export class AngularDirectivesExplanationPage {
 
-  constructor() { }
+  constructor() { 
 
-  ngOnInit() {
   }
 
+  
+
+  //Show the "ClickMe" Button
+  showClickMeButton() {
+    document.getElementById("clickMeButton").style.display = "block"
+    document.getElementById("clickMeButton").style.animationPlayState = "running";
+  }
 }
