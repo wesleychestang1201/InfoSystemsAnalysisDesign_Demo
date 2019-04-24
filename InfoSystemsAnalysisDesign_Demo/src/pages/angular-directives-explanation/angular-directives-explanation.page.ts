4ad56831e350dba4ad7ef6841a49ce0c71723eba
Wesley Chestang <wesley.chestang@terraforms.net>
2019-04-24 17:19:25 -0500

    Follow up saved To previous commit

--- InfoSystemsAnalysisDesign_Demo/src/pages/angular-directives-explanation/angular-directives-explanation.page.ts
+++ InfoSystemsAnalysisDesign_Demo/src/pages/angular-directives-explanation/angular-directives-explanation.page.ts
@@ -15,44 +15,55 @@ export class AngularDirectivesExplanationPage {
 
 
   //The constructor is used to instantiate any variables or representations of imports such as alert controllers
-  constructor (
+  constructor(
     public toastController: ToastController,
-    private router: Router) { 
-    }
-    
-
-
-    // []][][][][]][][][][][][][[]][][][][][][][][]]][][][][][][][][][][][][][][][][]]][][][]][][][][][]][]]][][]][]
-  
-  
-
-   //Navigate to the "Angular Directives Demo" page or "Angular Directives Demo Thirty Min" respective to the toggled choice
-   navigateToDemoPage() {
-     if (this.demoTypeSelection) {
-       //Navigate to the 15 min demo if chosen
-       if (this.demoTypeSelection == 15) {
-        this.router.navigate(['angular-directives-demo']);
-       }
-        //Navigate to the 30 min demo if chosen 
-       if (this.demoTypeSelection == 30) {
-        this.router.navigate(['angular-directives-demo-thirty-min']);
-       }
-     } else {
-       this.displayMessage()
-     }
+    private router: Router) {
   }
 
-      //Display the popup message if the user did not select a demonstration length
-      async displayMessage() {
-        const toast = await this.toastController.create({
-          message: 'Please Select a Demonstration Length!',
-          position: "middle",
-          duration: 2500
-        });
-        toast.present();
-      }
 
-  
+
+
+
+  // []][][][][]][][][][][][][[]][][][][][][][][]]][][][][][][][][][][][][][][][][]]][][][]][][][][][]][]]][][]][]
+
+
+
+
+
+  //Navigate to the "Angular Directives Demo" page or "Angular Directives Demo Thirty Min" respective to the toggled choice
+  navigateToDemoPage() {
+    if (this.demoTypeSelection) {
+      //Navigate to the 15 min demo if chosen
+      if (this.demoTypeSelection == 15) {
+        this.router.navigate(['angular-directives-demo']);
+      }
+      //Navigate to the 30 min demo if chosen 
+      if (this.demoTypeSelection == 30) {
+        this.router.navigate(['angular-directives-demo-thirty-min']);
+      }
+    } else {
+      this.displayMessage()
+    }
+  }
+
+
+
+
+  //#region PopupMessage
+  //Display the popup message if the user did not select a demonstration length
+  async displayMessage() {
+    const toast = await this.toastController.create({
+      message: 'Please Select a Demonstration Length!',
+      position: "middle",
+      duration: 2500
+    });
+    toast.present();
+  }
+  //#endregion
+
+
+
+
   //Show the "DemoTypeSelection Controls" Button
   showDemoTypeSelection() {
     //Change the element display from "none" to "block", making it render on the view
